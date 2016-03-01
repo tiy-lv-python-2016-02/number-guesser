8d31f58c494f21eae8516df763d40a26b11d913e
Aaron Scruggs <aarondscruggs@gmail.com>
2016-03-01 13:38:50 -0800

    Base game working

--- NumberGuesser.py
+++ NumberGuesser.py
@@ -2,37 +2,43 @@
 
 import random
 
-print("The computer has selected a number from 1 to 100. You have 5 tries to guess it")
+# Add different options?
+
+print("",
+      "The computer has selected a number from 1 to 100. You have 5 tries to guess it",
+      sep="\n"
+      )
 
 secret_number = random.randint(1, 100)
 chances = 5
 
-for counter in range(chances):
-    print("You have {} guesses remaining".format(chances))
-    guess = ""
+# Tell if close
+# docstring
 
-    while not guess.isdigit():
-        guess = input("What is your guess? ")
+def game(chances, secret_number):
+    for counter in range(chances):
+        print("You have {} guesses remaining".format(chances - counter), "", sep="\n")
+        guess = ""
 
-    guess = int(guess)
+        while not guess.isdigit():
+            guess = input("What is your guess? ")
 
-    if guess == secret_number:
-        print("You win!")
-        print("It took you {} tries".format(counter + 1))
-        break
-    elif guess < secret_number:
-        print("You guessed too low")
-    else:
-        print("You guessed too high")
+        guess = int(guess)
+        print("")
 
-# loop through chances
-# feedback: chances left, high/low, (close)
-#
+        if guess == secret_number:
+            print("You win!")
+            print("It took you {} tries".format(counter + 1))
+            return
+        elif guess < secret_number:
+            print("You guessed too low")
+        else:
+            print("You guessed too high")
 
+        if abs(guess - secret_number) < 5:
+            print("You are close!")
+    print("You have run out of guesses. The secret number was {}".format(secret_number))
 
-# for _ in  range(chances):
-# break if correct
-
-# if chances too high, losing message
+game(chances, secret_number)
 
 
